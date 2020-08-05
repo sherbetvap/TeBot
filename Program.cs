c85bcd421089a278e8c3b5133e53f9fc4f2aae07
Ivan <haunter50@gmail.com>
2020-08-04 18:54:00 -0700

    Move json and db into parent folder. Attempts to reconnect if disconnected for 3 minutes.

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Reflection;
+using System.Timers;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Json;
@@ -21,6 +22,8 @@ namespace TeBot
         private DiscordSocketClient client;
         private CommandHandler commandHandler;
         private CommandService service;
+        private Timer timer;
+        private static string dataLocation = (new FileInfo(AppDomain.CurrentDomain.BaseDirectory)).Directory.Parent.FullName;
 
         public static void Main(string[] args)
             => new Program().MainAsync().GetAwaiter().GetResult();
@@ -29,8 +32,10 @@ namespace TeBot
         {
 #if DEBUG
             string jsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"stagingconfig.json");
+
 #else
-            string jsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"config.json");            
+            Console.WriteLine("Looking for Json and DB in: " + dataLocation);
+            string jsonPath = Path.Combine(dataLocation, @"stagingconfig.json");
 #endif
             // create the configuration
             var builder = new ConfigurationBuilder()
@@ -42,17 +47,18 @@ namespace TeBot
         }
 
         public async Task MainAsync()
-        {
-            SQLiteConnection sqlite;
-            sqlite = CreateConnection();
-
+        {            
             client = new DiscordSocketClient();
             service = new CommandService(new CommandServiceConfig
             {                                       // Add the command service to the collection
                 LogLevel = LogSeverity.Verbose,     // Tell the logger to give Verbose amount of info
                 DefaultRunMode = RunMode.Async,     // Force all commands to run async by default
             });
-            commandHandler = new CommandHandler(client, service, config, sqlite);
+            SQLManager sQLManager = new SQLManager(CreateConnection());
+
+            commandHandler = new CommandHandler(client, service, config, sQLManager);
+
+            client.Disconnected += OnDisconnected;           
 
             client.Log += Log;
             await client.LoginAsync(TokenType.Bot, config["Token"]);
@@ -65,7 +71,7 @@ namespace TeBot
         {
             SQLiteConnection sqlite_conn;
             // Create a new database connection:
-            sqlite_conn = new SQLiteConnection("Data Source=TeDB.db;Version=3;New=False;Compress=True;");
+            sqlite_conn = new SQLiteConnection("Data Source=" + Path.Combine(dataLocation, "TeDB.db") + ";Version=3;New=False;Compress=True;");
             // Open the connection:
             try
             {
@@ -83,6 +89,35 @@ namespace TeBot
             Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
         }
+
+        /// <summary>
+        /// When disconnected, wait for X minutes to reconnect. If not connected again then restart connection attempt. 
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <returns></returns>
+        private Task OnDisconnected(Exception arg)
+        {
+            timer = new Timer(3 * 60 * 1000) { AutoReset = true };
+            timer.Elapsed += async (object sender, ElapsedEventArgs e) => {
+                //if not connected after waiting 3 minutes
+                if (client.ConnectionState != ConnectionState.Connected)
+                {
+                    await client.StopAsync();
+                    await client.StartAsync();
+                }
+            };
+
+            //while still not connected try to connect
+            while (client.ConnectionState != ConnectionState.Connected)
+            {
+                timer.Enabled = true;
+            }
+            //end timer after connecting again
+            timer.Enabled = false;
+            
+            return Task.CompletedTask;
+        }
+
     }
 }
     
